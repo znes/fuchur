ecfd8c554b8502bb53dab25db809c43852d3931b
simnh <simon.hilpert@uni-flensburg.de>
2018-12-13 18:39:14 +0100

    Prepare PyPi release

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,4 +5,4 @@ Changelog
 0.0.0 (2018-12-13)
 ------------------
 
-* First release on PyPI.
+* First release on PyPI. Just skeleton - no functionality yet.
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
