4e81bdabb3ac14e547d68e727f02851e4917fb37
Stephan Günther <gnn.code@gmail.com>
2019-01-15 17:02:15 +0100

    Wrap overly long line

--- setup.py
+++ setup.py
@@ -41,7 +41,10 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # complete classifier list:
+        #
+        #   http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
