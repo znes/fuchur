054edbcc0ed9b90215fd29b3a52b4680e20030aa
Stephan Günther <gnn.code@gmail.com>
2019-01-16 02:25:48 +0100

    Fix `cli` test

--- tests/test_fuchur.py
+++ tests/test_fuchur.py
@@ -1,12 +1,13 @@
 
 from click.testing import CliRunner
 
-from fuchur.cli import main
+from fuchur.cli import cli
 
 
 def test_main():
     runner = CliRunner()
-    result = runner.invoke(main, [])
+    result = runner.invoke(cli, [])
+    help = runner.invoke(cli, ["--help"])
 
-    assert result.output == '()\n'
+    assert result.output == help.output
     assert result.exit_code == 0
